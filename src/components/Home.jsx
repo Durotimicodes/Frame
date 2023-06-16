2a741398738cd96030442f13cd486dd62d723da4
edmond fagbuyi <edmondfagbuyi@gmail.com>
2023-06-16 15:52:36 +0100

    about page

---
+++ src/components/Home.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import {HiArrowNarrowRight} from 'react-icons/hi'
+
+const Home = () => {
+  return (
+    <div name='home' className= 'w-full h-screen bg-[#0a192f]'>
+
+    {/* Container */}
+    <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+        <p className='text-pink-600 font-bold'>Hi, my name is </p>
+        <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Edmond Fagbuyi</h1>
+        <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Cloud and Web Developer</h2>
+        <p className='text-[#8892b0] py-4 max-w-[700px]'>A highly skilled and dedicated Cloud and Web Developer with almost 3 years of experience in developing
+scalable and robust web applications. Seeking a challenging position to utilize my expertise in front
+end and back end development, database manage ment, and problem solving to contribute to the
+success of a dynamic organization. Highly motivated to learn new conc ept and technologies.</p>
+<button className= 'max-w-[170px] group text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
+        View Work
+        <span className='group-hover:rotate-90  duration-300'><HiArrowNarrowRight className='ml-3'/></span>
+    </button>
+    </div>
+    
+
+
+</div>
+  )
+}
+
+export default Home
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import Logo from '../assests/logo1.png'
-import {FaBars, FaTimes} from 'react-icons/fa'
+import React, {useState} from 'react';
+import Logo from '../assests/logo1.png';
+import {FaBars, FaTimes, FaGithub, FaLinkedin} from 'react-icons/fa';
+import {HiOutlineMail} from 'react-icons/hi'
+import {BsFillPersonLinesFill} from 'react-icons/bs'
 
 const Navbar = () => {
+
+const [nav, setNav] = useState(false)
+const handleClick = () => setNav(!nav)
+
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
         <div>
-            <img src={Logo} alt='Logo Image' style={{width: '50px'}}/>
+            <img src={Logo} alt='Logo
+            ' style={{width: '50px'}}/>
         </div>
 
         {/* Menu */}
@@ -20,24 +27,30 @@ const Navbar = () => {
             </ul>
 
         {/* Hamburger */}
-        <div className='md:hidden'>
-            <FaBars/>
+        <div onClick={handleClick} className='md:hidden z-10'>
+            {!nav ? <FaBars/> : <FaTimes/>}
         </div>
 
         {/* Mobile menu */}
-        <div>
-        <ul className='absolute top-0 left-0 w-full h-screen bg-[#0a192f]'>
-            <li>Home</li>
-            <li>About</li>
-            <li>Skills</li>
-            <li>Work</li>
-            <li>Certificates</li>
-            <li>Contact</li>
+         <ul className={!nav ? 'hidden' :'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
+            <li className='py-6 text-4xl'>Home</li>
+            <li className='py-6 text-4xl'>About</li>
+            <li className='py-6 text-4xl'>Skills</li>
+            <li className='py-6 text-4xl'>Work</li>
+            <li className='py-6 text-4xl'>Certificates</li>
+            <li className='py-6 text-4xl'>Contact</li>
         </ul>
-        </div>
 
         {/* Social icons */}
-        <div className='hidden'></div>
+        <div className='hidden md:flex fixed flex-col top-[35%] left-0'>
+        <ul>
+        <li className='w-[160px] h-[60px] justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'><a href='https://www.linkedin.com/in/oluwadurotimi-fagbuyi-08a941142/' className='flex justify-between items-center w-full text-gray-300'>LinkedIn <FaLinkedin size={30}/></a></li>
+        <li className='w-[160px] h-[60px] justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'><a href='https://github.com/durotimicodes' className='flex justify-between items-center w-full text-gray-300'>Github <FaGithub size={30}/></a></li>
+        <li className='w-[160px] h-[60px] justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#EF2632]'><a href='edmondfagbuyi@gmail.com' className='flex justify-between items-center w-full text-gray-300'>Email <HiOutlineMail size={30}/></a></li>
+        <li className='w-[160px] h-[60px] justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#EEBC1D]'><a href='/' className='flex justify-between items-center w-full text-gray-300'>Resume <BsFillPersonLinesFill size={30}/></a></li>
+        </ul>
+
+        </div>
     </div>
   )
 }
