e9e54733ae24d12901cb05c0970bdabe72cf887e
edmond fagbuyi <edmondfagbuyi@gmail.com>
2023-06-16 19:15:06 +0100

    refactoring

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,8 @@ import Logo from '../assests/logo1.png';
 import {FaBars, FaTimes, FaGithub, FaLinkedin} from 'react-icons/fa';
 import {HiOutlineMail} from 'react-icons/hi'
 import {BsFillPersonLinesFill} from 'react-icons/bs'
+import {Link} from 'react-scroll'
+
 
 const Navbar = () => {
 
@@ -18,12 +20,12 @@ const handleClick = () => setNav(!nav)
 
         {/* Menu */}
             <ul className='hidden md:flex'>
-                <li>Home</li>
-                <li>About</li>
-                <li>Skills</li>
-                <li>Work</li>
-                <li>Certificates</li>
-                <li>Contact</li>
+                <li><Link to="home" spy={true} smooth={true}  duration={500}>Home</Link></li>
+                <li> <Link to="about" spy={true} smooth={true}  duration={500}>About</Link></li>
+                <li> <Link to="skills" spy={true} smooth={true}  duration={500}>Skills</Link></li>
+                <li> <Link to="projects" spy={true} smooth={true}  duration={500}>Projects</Link></li>
+                <li> <Link to="certificates" spy={true} smooth={true}  duration={500}>Certificates</Link></li>
+                <li> <Link to="contact" spy={true} smooth={true}  duration={500}>Contact</Link></li>
             </ul>
 
         {/* Hamburger */}
@@ -44,10 +46,10 @@ const handleClick = () => setNav(!nav)
         {/* Social icons */}
         <div className='hidden md:flex fixed flex-col top-[35%] left-0'>
         <ul>
-        <li className='w-[160px] h-[60px] justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'><a href='https://www.linkedin.com/in/oluwadurotimi-fagbuyi-08a941142/' className='flex justify-between items-center w-full text-gray-300'>LinkedIn <FaLinkedin size={30}/></a></li>
-        <li className='w-[160px] h-[60px] justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'><a href='https://github.com/durotimicodes' className='flex justify-between items-center w-full text-gray-300'>Github <FaGithub size={30}/></a></li>
-        <li className='w-[160px] h-[60px] justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#EF2632]'><a href='edmondfagbuyi@gmail.com' className='flex justify-between items-center w-full text-gray-300'>Email <HiOutlineMail size={30}/></a></li>
-        <li className='w-[160px] h-[60px] justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#EEBC1D]'><a href='/' className='flex justify-between items-center w-full text-gray-300'>Resume <BsFillPersonLinesFill size={30}/></a></li>
+        <li className='w-[160px] h-[60px] justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'><a href='https://www.linkedin.com/in/oluwadurotimi-fagbuyi-08a941142/' className='flex justify-between items-center w-full text-gray-300 font-bold'>LinkedIn <FaLinkedin size={30}/></a></li>
+        <li className='w-[160px] h-[60px] justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'><a href='https://github.com/durotimicodes' className='flex justify-between items-center w-full text-gray-300 font-bold'>Github <FaGithub size={30}/></a></li>
+        <li className='w-[160px] h-[60px] justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#EF2632]'><a href='/Contact' className='flex justify-between items-center w-full text-gray-300 font-bold'>Email <HiOutlineMail size={30}/></a></li>
+        <li className='w-[160px] h-[60px] justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#EEBC1D]'><a href='/assests/documents/Edmond_Fagbuyi_Resume.pdf' className='flex justify-between items-center w-full text-gray-300 font-bold'>Resume <BsFillPersonLinesFill size={30}/></a></li>
         </ul>
 
         </div>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -9,6 +9,11 @@ import Python from '../assests/Python-Logo-Free-Download-PNG.png';
 import GitHub from '../assests/github.png';
 import Tailwind from '../assests/tailwind.png';
 import Mongo from '../assests/mongo.png';
+import Postgresql from '../assests/Postgresql_elephant.png';
+import Mysql from '../assests/MySQL-Logo.png';
+import AWS from '../assests/aws.png'
+
+
 
 const Skills = () => {
   return (
@@ -30,8 +35,8 @@ const Skills = () => {
                   <p className='my-4'>CSS</p>
               </div>
               <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={JavaScript} alt="HTML icon" />
-                  <p className='my-4'>JAVASCRIPT</p>
+                  <img className='w-20 mx-auto' src={Tailwind} alt="HTML icon" />
+                  <p className='my-4'>TAILWIND</p>
               </div>
               <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                   <img className='w-20 mx-auto' src={ReactImg} alt="HTML icon" />
@@ -42,21 +47,35 @@ const Skills = () => {
                   <p className='my-4'>GITHUB</p>
               </div>
               <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={JavaScript} alt="HTML icon" />
+                  <p className='my-4'>JAVASCRIPT</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                   <img className='w-20 mx-auto' src={Golang} alt="HTML icon" />
                   <p className='my-4'>GOLANG</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={Mongo} alt="HTML icon" />
-                  <p className='my-4'>MONGO DB</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={Tailwind} alt="HTML icon" />
-                  <p className='my-4'>TAILWIND</p>
               </div>
               <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                   <img className='w-20 mx-auto' src={Python} alt="HTML icon" />
                   <p className='my-4'>PYTHON</p>
               </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={AWS} alt="HTML icon" />
+                  <p className='my-4'>AWS</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={Mongo} alt="HTML icon" />
+                  <p className='my-4'>MONGO DB</p>
+              </div>
+           
+             <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={Postgresql} alt="HTML icon" />
+                  <p className='my-4'>POSTGRESQL</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={Mysql} alt="HTML icon" />
+                  <p className='my-4'>MYSQL</p>
+              </div>
+              
           </div>
       </div>
     </div>
